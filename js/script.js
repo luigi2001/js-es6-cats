f57a9070019b0df1a0218e9ba821e645eab86b88
luigi2001 <luigitanga@virgilio.it>
2021-06-17 19:33:05 +0200

    add Milestone 2 - Milestone 3

--- js/script.js
+++ js/script.js
@@ -22,7 +22,7 @@ const cats = [
     },
     {
         name : 'Kitty',
-        eta : 10,
+        eta : 3,
         color : '#ffff00',
         sesso : 'F'
     }
@@ -32,5 +32,53 @@ cats.forEach((element) => {
     document.getElementById('cats').innerHTML += 
     `
     ${element.name} <i class="fas fa-cat" style = "color:${element.color};"></i>
+    `
+});
+
+
+/* Milestone 2 */
+
+const catsM = [];
+
+const catsF = [];
+
+cats.forEach((element) => {
+    const {name,eta,color,sesso} = element;
+    const fioccoF = '#ffc0cb';
+    const fioccoM = '#0000ff';
+    if(element.sesso == 'M'){
+        catsM.push({
+            name,
+            eta,
+            color,
+            sesso,
+            fiocco : fioccoM
+        });
+    }else{
+        catsF.push({
+            name,
+            eta,
+            color,
+            sesso,
+            fiocco : fioccoF
+        });
+    }
+});
+
+console.log(catsF);
+
+console.log(catsM);
+
+
+/* Milestone 3 */
+
+let catsNew = [...catsF,...catsM];
+
+
+catsNew.forEach((element) => {
+    document.getElementById('cats2').innerHTML += 
+    `
+    ${element.name} <i class="fas fa-cat" style = "color:${element.color};"></i>
+    <i class="fas fa-paw" style = "color:${element.fiocco};"></i>
     `
 });
