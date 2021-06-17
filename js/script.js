4cd6176ade6627824f3218093d8cb5b258ab9df3
luigi2001 <luigitanga@virgilio.it>
2021-06-17 15:28:55 +0200

    add Milestone 1

--- js/script.js
+++ js/script.js
@@ -1,2 +1,31 @@
 
-console.log('ciao');
+/* Milestone 1 */
+
+const cats = [
+    {
+        name : 'Garfild',
+        eta : 5,
+        color : '#ffa500',
+        sesso : 'M'
+    },
+    {
+        name : 'Felix',
+        eta : 7,
+        color : '#FFFFFF',
+        sesso : 'M'
+    },
+    {
+        name : 'Jerry',
+        eta : 10,
+        color : '#000000',
+        sesso : 'M'
+    },
+    {
+        name : 'Kitty',
+        eta : 10,
+        color : '#FFFFFF',
+        sesso : 'F'
+    }
+];
+
+cats.forEach((element) => console.log(element.name, element.color));
